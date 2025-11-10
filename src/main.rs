17d63fffaa4adc22f26cbb516e0d9dc06b2fbe63
Afonso Miguel <afonso.miguel@pucpr.br>
2025-11-10 17:56:14 -0300

    Update from professor

--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,29 @@ fn main() {
     // Esta thread vai dar panic! e envenenar o mutex
     let handle = thread::spawn(move || {
         let guard = data_clone.lock().unwrap();
-        panic!("A thread 1 falhou!"); // O lock é envenenado
+       // panic!("A thread 1 falhou!"); // O lock é envenenado
         *guard + 1 
     }); // Espera a thread falhar
 
     match handle.join() {
         Ok(result) => println!("Thread 1 result: {}", result),
-        Err(_) => println!("A thread 1 falhou."),
+        Err(_) => {
+            println!("A thread 1 falhou.");
+            match data.lock() {
+                Ok(r) => {
+                    println!("Ok, falhou mas continua íntegro: {}", *r);
+                },
+                Err(e) => {
+                    println!("Que pena! A thread falhou e o Mutex está envenenado!");
+                    println!("Mesmo assim, aqui está seu valor: {}", e.into_inner());
+                }
+            }
+        }
     }
 
     // A thread 'main' tenta travar o mutex envenenado
-    let res = data.lock(); // Não usamos .unwrap()
+    //let res = data.lock(); // Não usamos .unwrap()
 
     // 'res' será um Err(PoisonError(...))
-    println!("Mutex está envenenado? {}", res.is_err()); // true
+    //println!("Mutex está envenenado? {}", res.is_err()); // true
 }
